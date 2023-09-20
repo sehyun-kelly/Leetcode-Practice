5da09446c23facc6cecdbde417bb9de2f28b3e8f
Kelly Park <sehyun.kelly.park@gmail.com>
2023-09-19 17:57:05 -0700

    091923 234 completed

---
+++ Others/234_Palindrome_Linked_List.java
@@ -0,0 +1,30 @@
+/** 32.59%, 35.43% */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+
+        while(head != null) {
+            list.add(head.val);
+
+            head = head.next;
+        }
+
+        int l = 0, r = list.size() - 1;
+
+        while(l <= r){
+            if(list.get(l++) != list.get(r--)) return false;
+        }
+
+        return true;
+    }
+}
